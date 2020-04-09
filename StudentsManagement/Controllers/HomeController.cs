929127e26ff7b5c87b608b064e95f4ef0a7c6eed
Mr_Pengchangchun <269666877@qq.com>
2020-04-09 12:34:45 +0800

    添加项目文件。

---
+++ StudentsManagement/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentsManagement.Models;
+using StudentsManagement.ViewModels;
+
+namespace StudentManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+        //构造函数的注入的方式注入IStudentRepository
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        //[Route("")]
+        //[Route("Home")]
+        //[Route("Home/Index")]
+        public IActionResult Index()
+        {
+           IEnumerable<Student> students =_studentRepository.GetAllStudents();
+            return View(students);
+        }
+        //[Route("Home/Details/{id?}")]
+        public IActionResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Student = _studentRepository.GetStudent(id??1),
+                PageTitle = "学生详细信息",
+             };
+            return View(homeDetailsViewModel);
+        }
+        public IActionResult Create()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+    }
+}
---
+++ StudentsManagement/Models/MockStudentRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsManagement.Models
+{
+    /// <summary>
+    /// 模拟类
+    /// </summary>
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _studentsList;
+        /// <summary>
+        /// 数据模拟
+        /// </summary>
+        public MockStudentRepository()
+        {
+            _studentsList = new List<Student>()
+            {
+                new Student(){Id=1,Name="张三",ClassName=ClassNameEnum.FirstGrade,Email="123@163.com"},
+                new Student(){Id=2,Name="李四",ClassName=ClassNameEnum.FirstGrade,Email="456@163.com"},
+                new Student(){Id=3,Name="王五",ClassName=ClassNameEnum.FirstGrade,Email="789@163.com"},
+            };
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return _studentsList;
+        }
+
+        public Student GetStudent(int id)
+        {
+            return _studentsList.FirstOrDefault(a => a.Id == id);
+        }
+    }
+}
